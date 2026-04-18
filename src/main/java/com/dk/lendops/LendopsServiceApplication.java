f9d0c4db8b1cdb3c080d34b3a914dfaf4c2534c0
David <build.davidkariuki@gmail.com>
2026-04-18 16:51:20 +0300

    config: Project initialization
    Signed-off-by: David <build.davidkariuki@gmail.com>

---
+++ src/main/java/com/dk/lendops/LendopsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dk.lendops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendopsServiceApplication.class, args);
+	}
+
+}
