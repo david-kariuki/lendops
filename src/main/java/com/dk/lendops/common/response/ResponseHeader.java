711712844693c5e6516ea6d2c2b729fb7d5dd656
David <build.davidkariuki@gmail.com>
2026-04-19 11:28:09 +0300

    feat: Implement global response handling
    Signed-off-by: David <build.davidkariuki@gmail.com>

---
+++ src/main/java/com/dk/lendops/common/response/ResponseHeader.java
@@ -0,0 +1,23 @@
+package com.dk.lendops.common.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * API response header
+ *
+ * @author David Kariuki
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResponseHeader {
+
+    private String requestRefId;
+    private int responseCode;
+    private String technicalMessage;
+    private String customerMessage;
+    private LocalDateTime timestamp;
+}
