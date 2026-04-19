02a381132f288e487a31dc6fb4be7aaea305c44f
David <build.davidkariuki@gmail.com>
2026-04-19 11:36:27 +0300

    feat: Implement product configuration functionality
    Signed-off-by: David <build.davidkariuki@gmail.com>

---
+++ src/main/java/com/dk/lendops/product/enums/ConfigType.java
@@ -0,0 +1,14 @@
+package com.dk.lendops.product.enums;
+
+/**
+ * ConfigType
+ *
+ * @author David Kariuki
+ */
+public enum ConfigType {
+
+    TENURE,
+    FEES,
+    BILLING,
+    LOAN_STRUCTURE
+}
