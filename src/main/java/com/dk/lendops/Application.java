bd0905bf923f45ed97e8b939c5eada5b7ca5ded6
David <build.davidkariuki@gmail.com>
2026-04-19 11:31:19 +0300

    refactor: Rename application class
    Signed-off-by: David <build.davidkariuki@gmail.com>

--- src/main/java/com/dk/lendops/Application.java
+++ src/main/java/com/dk/lendops/Application.java
@@ -3,11 +3,15 @@ package com.dk.lendops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application class
+ *
+ * @author David Kariuki
+ */
 @SpringBootApplication
-public class LendopsServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LendopsServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
